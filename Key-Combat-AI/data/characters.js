02ae18a3807246cc5d6ea8bbb7bca6c72a96b0a5
HaloMathieu <94016222+HaloMathieu@users.noreply.github.com>
2025-08-13 15:45:48 -0500

    Add new characters with image paths

--- Key-Combat-AI/data/characters.js
+++ Key-Combat-AI/data/characters.js
@@ -35,5 +35,77 @@ window.charactersData = [
     key: 't',
     ultChargeNeeded: 6,
     ultEffect: 'Unleashes a flurry of strikes'
+  },
+  {
+    id: 'hero_azal',
+    name: 'Azal',
+    type: 'Attack',
+    baseHP: 90,
+    baseAttack: 22,
+    passive: 'Infernal Surge: Bonus damage when at low HP',
+    key: 'y',
+    ultChargeNeeded: 5,
+    ultEffect: 'Unleashes a burst of flame',
+    image: 'assets/images/heroes/Azal_character.png'
+  },
+  {
+    id: 'hero_fyra',
+    name: 'Fyra',
+    type: 'Support',
+    baseHP: 85,
+    baseAttack: 15,
+    passive: 'Flame Ward: Shields allies briefly',
+    key: 'u',
+    ultChargeNeeded: 4,
+    ultEffect: 'Restores ally health over time',
+    image: 'assets/images/heroes/fyra_character.png'
+  },
+  {
+    id: 'hero_lucien',
+    name: 'Lucien',
+    type: 'Attack',
+    baseHP: 95,
+    baseAttack: 23,
+    passive: 'Blade Dance: Small chance for extra strike',
+    key: 'i',
+    ultChargeNeeded: 6,
+    ultEffect: 'Performs a deadly combo',
+    image: 'assets/images/heroes/lucien_character.png'
+  },
+  {
+    id: 'hero_raeni',
+    name: 'Raeni',
+    type: 'Support',
+    baseHP: 75,
+    baseAttack: 12,
+    passive: 'Wind Blessing: Increases ally speed',
+    key: 'o',
+    ultChargeNeeded: 5,
+    ultEffect: 'Summons a protective gale',
+    image: 'assets/images/heroes/raeni_character.png'
+  },
+  {
+    id: 'hero_velra',
+    name: 'Velra',
+    type: 'Attack',
+    baseHP: 100,
+    baseAttack: 18,
+    passive: 'Stone Skin: Reduces incoming damage slightly',
+    key: 'p',
+    ultChargeNeeded: 5,
+    ultEffect: 'Slams the ground for area damage',
+    image: 'assets/images/heroes/velra_character.png'
+  },
+  {
+    id: 'hero_zhae',
+    name: 'Zhae',
+    type: 'Attack',
+    baseHP: 85,
+    baseAttack: 26,
+    passive: 'Shadowstep: Dodges one attack periodically',
+    key: 'a',
+    ultChargeNeeded: 6,
+    ultEffect: 'Strikes from the shadows',
+    image: 'assets/images/heroes/zhae_character.png'
   }
 ];
